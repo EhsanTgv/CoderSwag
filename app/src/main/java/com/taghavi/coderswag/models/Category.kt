1432b92e880c8965b8a1543e62808a246d32a3f3
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-03-02 21:58:39 +0330

    Category Model, toString function rewrite

--- app/src/main/java/com/taghavi/coderswag/models/Category.kt
+++ app/src/main/java/com/taghavi/coderswag/models/Category.kt
@@ -1,3 +1,7 @@
 package com.taghavi.coderswag.models
 
-class Category(val title:String,val image:String)
+class Category(private val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
